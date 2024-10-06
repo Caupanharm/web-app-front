14dd988a3b2002ae3b4ea8fb8f9e510da871f045
Matéo Fontanel <fontanelmateo@gmail.com>
2024-10-06 13:02:55 +0200

    Fixed page nav buttons

--- src/components/NavButtons.tsx
+++ src/components/NavButtons.tsx
@@ -4,6 +4,7 @@ import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { NavButtonsProps } from "../interfaces/ComponentsInterfaces";
 import { Button, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+
 export default function NavButtons({
   page,
   setPage,
@@ -14,15 +15,16 @@ export default function NavButtons({
   const [prevButtonState, setPrevButtonState] = useState<boolean>(true);
   const [nextButtonState, setNextButtonState] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (page == 0) setPrevButtonState(false)
-    if (dataSize > page * pageSize) setNextButtonState(true);
-  }, []);
+  const lastPageSize = dataSize % pageSize
+  const numberOfPages = (lastPageSize > 0) ? Math.floor(dataSize / pageSize) + 1 : Math.floor(dataSize / pageSize)
+
+  useEffect(() => { // First render and everytime the page changes
+    setPrevButtonState(page > 0)
+    setNextButtonState(page < Math.floor(dataSize / pageSize));
+  }, [page]);
 
   const handleDisplay = (event) => {
     const requestedPage = (event.target.id === "prev") ? page - 1 : page + 1
-    const lastPageSize = dataSize % pageSize
-    const numberOfPages = (lastPageSize > 0) ? Math.floor(dataSize / pageSize) + 1 : Math.floor(dataSize / pageSize)
 
     if (requestedPage < 0) {
       setPage(0)
@@ -30,20 +32,18 @@ export default function NavButtons({
       setPage(numberOfPages - 1)
     } else {
       setPage(requestedPage)
-      setPrevButtonState(requestedPage > 0)
-      setNextButtonState(requestedPage < numberOfPages - 1)
     }
   };
 
   return (
     <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "8px" }}>
-      <Button id="prev" 
-      sx={{color: theme.palette.secondary.main, width: "12%", justifyContent: "left" }}
-      onClick={handleDisplay} disabled={!prevButtonState} variant="contained" startIcon={<NavigateBeforeIcon />} size="large"> Page précédente</Button>
+      <Button id="prev"
+        sx={{ color: theme.palette.secondary.main, justifyContent: "left" }}
+        onClick={handleDisplay} disabled={!prevButtonState} variant="contained" startIcon={<NavigateBeforeIcon />} size="large">Page précédente</Button>
       <Typography sx={{ color: theme.palette.text.secondary, marginRight: "16px" }}>Page {page + 1}/{Math.floor(dataSize / pageSize) + 1}</Typography>
-      <Button id="next" 
-      sx={{color: theme.palette.secondary.main, width: "12%", justifyContent: "right" }}
-      onClick={handleDisplay} disabled={!nextButtonState} variant="contained" endIcon={<NavigateNextIcon />} size="large"> Page suivante</Button>
+      <Button id="next"
+        sx={{ color: theme.palette.secondary.main, justifyContent: "right" }}
+        onClick={handleDisplay} disabled={!nextButtonState} variant="contained" endIcon={<NavigateNextIcon />} size="large">Page suivante</Button>
     </div>
   );
 }
